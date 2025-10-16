cdcb9e4b11e16046b04e43433bb09bf27e028aa5
nails8919 <shorty_love_12@hotmail.com>
2025-10-15 21:06:35 -0400

    Final Commit

--- src/components/EmployeeCard3.tsx
+++ src/components/EmployeeCard3.tsx
@@ -8,7 +8,7 @@ export default function EmployeeCard3(){
       <div className="bg-pink-500 text-white p-4 w-full rounded-r-lg rounded-tl-lg text-center">
         {bio}
       </div>
-      <div className="bg-pink-100 p-3 rounded-b-lg border border-pink-300 -mt-0 ml-0 shadow-sm">
+      <div className="bg-pink-100 p-3 rounded-b-lg border border-pink-300 shadow-sm">
         <h2 className="text-pink-700 font-medium">{name}</h2>
         <p className="text-sm text-pink-600">{title}</p>
       </div>
